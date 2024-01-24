fa3b03406aac753eb402823231fda0cfab40500d
unknown <emgcalangian@gmail.com>
2024-01-24 22:11:35 +0800

    reverse printed

--- palindrome_checker.py
+++ palindrome_checker.py
@@ -8,5 +8,8 @@ original = input("Please input your desired number: ")
 # create an empty variable to hold the reverse number
 reverse = ""
 # use for loop to reverse
+for i in range(len(original), 0, -1):
+    reverse += original[i - 1]
 
+print("please print", reverse)
 # use if function to check if the number is a palindrome
