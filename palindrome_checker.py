e3d4394b7e8138d7f918de6c15906e7468103afa
unknown <emgcalangian@gmail.com>
2024-01-24 22:01:30 +0800

    pseudo code

---
+++ palindrome_checker.py
@@ -0,0 +1,11 @@
+# Write a program to check if the given number is a palindrome number.
+
+# pseudo code
+
+# ask user for an input number
+
+# create an empty variable to hold the reverse number
+
+# use for loop to reverse
+
+# use if function to check if the number is a palindrome
